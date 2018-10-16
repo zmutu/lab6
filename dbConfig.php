c20dcfe25b240d63844fbbffa7c72e2a30a0f71b
zmutu <43672768+zmutu@users.noreply.github.com>
2018-10-16 14:19:35 +0200

    Update dbConfig.php

--- dbConfig.php
+++ dbConfig.php
@@ -3,13 +3,13 @@ $lokal=0; // hodeian 0, lokalean gauedenean 1
 
 if ($lokal) {
    $zerbitzaria="localhost";
-   $erabiltzailea="id7270487_zmutu";
-   $gakoa="60ra3u5kalH3rr1a";				// GitHub-en eremu hau EZABATU
-   $db="id7270487_quiz";						// hodeiko db izena: hodeiko aurrizkia + zuek adierazitako db izena atzizki moduan
+   $erabiltzailea="idXXXXXXX_zmutu";
+   $gakoa="pasahitza";				// GitHub-en eremu hau EZABATU
+   $db="idXXXXXXX_quiz";						// hodeiko db izena: hodeiko aurrizkia + zuek adierazitako db izena atzizki moduan
 } else {
    $zerbitzaria="localhost";
    $erabiltzailea="root";						// lokalean erabiltzailea root izan ohi da
-   $gakoa="5artu";									// eta ez da pasahitzarik jartzen
+   $gakoa="5artu";									// eta ez da pasahitzarik jartzen//ba ni bazterrak nahasten ibili ondoren pasahitza behar dut
    $db="quiz";
 }
 %>
