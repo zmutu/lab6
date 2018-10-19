1ad962788b0feb8e044b47b1df3c93fa9c697afc
zmutu <43672768+zmutu@users.noreply.github.com>
2018-10-19 21:15:41 +0200

    Create dbCongif.php

---
+++ php/dbCongif.php
@@ -0,0 +1,16 @@
+<?php
+$leku=1; //0 hodeian eta 1 portatilan
+
+if($leku){
+	$zerbitzaria='localhost';
+	$erabiltzaile='root';
+	$gakoa='5artu';
+	$db='quiz';
+}
+else{
+	$zebitzaria='localhost';
+	$erabiltzaile='id?????_zmutu';
+	$gakoa='??????';
+	$db='id?????_quiz';
+}
+?>
