a5d136545921b36475e4ad5451f6a16e8d62127e
zmutu <43672768+zmutu@users.noreply.github.com>
2018-10-19 21:43:43 +0200

    Update showQuestions.php

--- php/showQuestions.php
+++ php/showQuestions.php
@@ -2,33 +2,45 @@
 //datu-basearekin konexioa egin
 include('dbConfig.php');
 //DBKS-rekin konexioa egin eta Quiz datu-basea ireki
-$konexioa = new mysqli($zerbitzaria,$erabiltzailea,$gakoa,$db);
+
+//mysql-rekin konexioa egin eta datu-basea aukeratu
+if(!$konexioa = new mysqli($zerbitzaria,$erabiltzaile,$gakoa,$db)){
+	header("location:mezua.php=goiburu=Errorea!!&gorputza=ez da konexioa lortu<br/>".$konexioa->error."&auk=<a href='addQuestion.html'>Idatzi beste galdera bat</a>");
+}
+
 $sql="Select * from questions";
+
 //sql kontsulta exekutatu
 if(!($emaitza=$konexioa->query($sql))){
 	header("location:mezua.php?goiburu=Errorea!!&gorputza=".$konexioa->error."&auk=-");
 }
 echo('<style>td{border:dotted 1px #5555AA;}</style>');
+
 $zutabe = $emaitza->field_count;
+
 echo('<table style="border:solid 1px #5555AA;"><tr>');
-//taularen goiburuak
-for($i=0;$i<$zutabe;$i++){
+
+//taularen goiburuak (marrazkia eta marrazki mota zutabeak ezik)
+for($i=0;$i<$zutabe-2;$i++){
     $emaitza->field_seek($i);
     $attr=$emaitza->fetch_field();
     echo("<th scope='row'>".$attr->name."</th>");
 }
 echo('</tr>');
-//taularen erregistroak
+
+//taularen erregistroak (marrazkirik gabe)
 for($k=0;$k<$emaitza->num_rows;$k++){
 	$lerro = $emaitza->fetch_array(MYSQLI_NUM);
 	echo('<tr>');
-	for($j=0;$j<$zutabe;$j++){
+	for($j=0;$j<$zutabe-2;$j++){
 	    echo('<td>'.$lerro[$j].'</td>');
 	}
 	echo('</tr>');
 }
 echo('</table>');
+
 $emaitza->free_result();
 $konexioa->close();
+
 echo('<a href="../layout.html">Galdera berri bat idatzi</a>');
 ?>
