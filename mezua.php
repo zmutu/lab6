4b68c4c6beb77a0112a7cd59f20fd2225b0aba0f
zmutu <43672768+zmutu@users.noreply.github.com>
2018-10-19 21:47:08 +0200

    Delete mezua.php

--- mezua.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(isset($_GET['goiburu'])){$goiburu=$_GET['goiburu'];}
-if(isset($_GET['gorputza'])){$gorputza=$_GET['gorputza'];}
-if(isset($_GET['auk'])){$auk=$_GET['auk'];}
-echo('<html><style>h1,p{align:center;padding:25px;text-align:center;}</style><body><h1>'.$goiburu.'</h1><p>'.$gorputza.'</p><p>'.$auk.'</p></body></html>');
-?>
