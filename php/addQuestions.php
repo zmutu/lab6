cdd829a1e72b083f1cea82ac98d8381d1327f0dc
zmutu <zmutuberria@gmail.com>
2018-10-21 17:33:49 +0200

    lab2a Bi a laborategiko php-ko fitxategiak

--- php/addQuestions.php
+++ php/addQuestions.php
@@ -8,7 +8,6 @@ if(isset($_GET['erantzunokerra2'])){$erantzunokerra2=$_GET['erantzunokerra2'];}
 if(isset($_GET['erantzunokerra3'])){$erantzunokerra3=$_GET['erantzunokerra3'];}
 if(isset($_GET['zailtasuna'])){$zailtasuna=$_GET['zailtasuna'];}
 if(isset($_GET['gaia'])){$gaia=$_GET['gaia'];}
-//if(isset($_GET[''])){$=$_GET[''];}
 
 //daturen bat falta den aztertu
 if($mail==''||$galdera==''||$zuzena==''||$erantzunokerra1==''||$erantzunokerra2==''||$erantzunokerra3==''||$zailtasuna==''||$gaia==''){
@@ -26,11 +25,11 @@ if(!$konexioa=new mysqli($zerbitzaria,$erabiltzaile,$gakoa,$db)){
 $sql="insert into Questions(mail,galdera,erantzun_zuzena,erantzun_okerra_1,erantzun_okerra_2,erantzun_okerra_3,zailtasuna,gaia) values('".$mail."','".$galdera."','".$zuzena."','".$erantzunokerra1."','".$erantzunokerra2."','".$erantzunokerra3."',".$zailtasuna.",'".$gaia."')";
 
 //sql kontsulta exekutatu
-if(!($emaitza = $konexioa -> query($sql))){
+if(!($emaitza=$konexioa->query($sql))){
 	header("location:mezua.php=goiburu=Errorea!!&gorputza=SQL kontsulta ez da exekutatu<br/>".$konexioa->error."&auk=<a href='addQuestion.html'>Idatzi beste galdera bat</a>");
 }
 //konexioa itxi
 $konexioa->close();
 
-header("location:mezua.php?goiburu=Eskaera gauzatu da&gorputza=Bidalitako datuak batu-basean ongi gorde dira&auk=<a href='../layout.html'>Orri nagusia</a><br/><a href='showQuestions.php'>Ikusi galdera guztiak (marrazkirik gabe)</a><br/><a href='addQuestionwithImage.php'>Ikusi galdera guztiak (marrazkiekin)</a>");
+header("location:mezua.php?goiburu=Eskaera gauzatu da&gorputza=Bidalitako datuak batu-basean ongi gorde dira&auk=<a href='../layout.html'>Orri nagusia</a><hr width='100'/><a href='showQuestions.php'>Ikusi galdera guztiak (marrazkirik gabe)</a>");
 ?>
